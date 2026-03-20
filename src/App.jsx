3c51ccdb9d59104297ee5ec5b254bc019a02977d
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2026-03-20 20:54:11 +0000

    chore: update deps and integrate Tailwind CSS with config setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import CGU from './pages/CGU';
-import Policy from './pages/Policy';
-import './style.css'; // Ton CSS global
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import CGU from "./pages/CGU";
+import Policy from "./pages/Policy";
+import "./style.css"; // Ton CSS global
 
 function App() {
   return (
